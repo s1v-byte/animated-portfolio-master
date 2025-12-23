47f7046a48eae296450975e7e2f55d82f5ea1536
Dave <surigaojohndave@gmail.com>
2025-12-23 21:26:44 +0800

    Portfolio Redux

--- animated-portfolio-master/js/script.js
+++ animated-portfolio-master/js/script.js
@@ -4,3 +4,11 @@ const mobileNavs = document.getElementsByClassName("nav-links")[0]
 hamburger.addEventListener("click", () => {
   mobileNavs.classList.toggle("active")
 })
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-track img');
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  track.style.transform = `translateX(-${index * 100}%)`;
+}, 3000);
